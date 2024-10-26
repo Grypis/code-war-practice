b0dd4533161af4888163da184d87edf6a43394ca
grypis <grypisnecros@gmail.com>
2024-10-26 21:22:35 +0300

    start

---
+++ js/8-kyu.js
@@ -0,0 +1,31 @@
+//! Task 1 - Completed
+/* function smash(words) {
+  return words.join(' ');
+} */
+/* const words = ['hello', 'world', 'this', 'is', 'great'];
+console.log(words.join(' ')); */
+
+/* words = ['hello', 'world', 'this', 'is', 'great'];
+smash(words); // returns "hello world this is great" */
+
+/* function smash(words) {
+  return words.join(' ');
+} */
+
+//! Task-2 - Completed
+/* function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+ */
+
+//! Task-3 - Completed
+// Write a function "greet" that returns "hello world!"
+/* function greet() {
+  return 'hello world!';
+} */
+
+//! Task-4 - Completed
+/* function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
+} */
